5caffd13a01da0a7e59b313aa721971b82836ded
dev-yohto <yohto089@gmail.com>
2024-06-13 22:24:21 +0900

    add 213_a, 247_b

---
+++ practice/B/abc247_b.py
@@ -0,0 +1,20 @@
+N = int(input())
+A = []
+
+for i in range(N):
+    full_name = input().split()
+    A.append(full_name)
+
+for i in range(N):
+    ctn_s = 0
+    ctn_t = 0
+    for j in range(N):
+        if A[j].count(A[i][0]) == 1:
+            ctn_s += 1
+        if A[j].count(A[i][1]) == 1:
+            ctn_t += 1
+        if ctn_s > 1 and ctn_t > 1:
+            print("No")
+            exit()
+
+print("Yes")
